7fb200cc6fb0a9a10c545e94a9a178c3538cc3c6
Anastasia Athans <64998016+USIDeaS@users.noreply.github.com>
2020-05-24 19:34:01 -0500

    myThirdCommit
    myThirdCommit

---
+++ A_Athans_burgerBuild/burgerBuild/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  burgerBuild
+//
+//  Created by Anastasia Athans-Stothoff on 5/21/20.
+//  Copyright © 2020 Anastasia Athans-Stothoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var whiteBread: UIButton!
+    @IBAction func whitePress(_ sender: Any)  {
+        
+    }
+    
+   
+    
+    @IBOutlet weak var baguetteBread: UIButton!
+  
+    @IBOutlet weak var wheatBread: UIButton!
+    @IBOutlet weak var fauxChixPatty: UIButton!
+    @IBOutlet weak var fauxBeefPatty: UIButton!
+    
+    @IBOutlet weak var pepperonciniVeggie: UIButton!
+    
+    @IBOutlet weak var eggplantVeggie: UIButton!
+    @IBOutlet weak var lettuceVeggie: UIButton!
+    
+    @IBOutlet weak var tomatoVeggie: UIButton!
+    
+    @IBOutlet weak var goatCheese: UIButton!
+    
+    @IBOutlet weak var fetaCheese: UIButton!
+    
+    
+    
+}
+
